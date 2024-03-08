a5cfe6c771d41c9912845fd1609833ef5868f5a1
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-03-08 22:07:19 +0530

    init commit

---
+++ src/shaders/vert.glsl
@@ -0,0 +1,20 @@
+#version 300 es
+
+layout (location=0) in vec4 position;
+
+uniform float uTime;
+uniform vec2 uResolution;
+uniform vec2 uMouse;
+uniform vec2 uPos;
+
+void main() {
+	gl_Position = position;
+    // gl_Position = vec4(position.x*cos(uTime*position.x),position.y*cos(uTime*position.y),position.z, 1.0);
+	// gl_Position = vec4(position.xy+uMouse, position.z, 1.0);
+	// gl_Position = vec4(position.xy+uPos, position.z, 1.0);
+
+    // vColor = color;
+    // vColor = vec3(sin(uTime*position.x),0.0,cos(uTime*position.y));
+    // vColor = vec3(abs(.5-position.xyz));
+    // vColor = vec3(abs(.5-position.x)*sin(uTime),0.0,abs(.5-position.z)*cos(uTime));
+}
